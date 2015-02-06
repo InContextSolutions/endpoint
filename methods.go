46324d89f1185598177802f5a367437a0b48dbd2
Johnny P. Neumann <arrantsquid@gmail.com>
2015-02-05 22:42:39 -0600

    first attempt

--- methods.go
+++ methods.go
@@ -1,6 +1,10 @@
 package endpoint
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 const (
 	// GET request
@@ -23,6 +27,10 @@ func get(ctx Context, h http.Handler) http.Handler {
 func post(ctx Context, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == POST {
+			var data map[string]interface{}
+			body, _ := ioutil.ReadAll(r.Body)
+			json.Unmarshal(body, &data)
+			ctx["data"] = data
 			h.ServeHTTP(w, r)
 		} else {
 			w.WriteHeader(http.StatusMethodNotAllowed)
