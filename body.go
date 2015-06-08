47fb455e6638b2a9b6426358ea2a0eab564252b1
Victor Brakauskas <victor.brakauskas@incontextsolutions.com>
2015-06-08 15:52:28 -0500

    removed json check

--- body.go
+++ body.go
@@ -10,12 +10,6 @@ func readBody() Middleware {
 	fn := func(ctx Context, h httprouter.Handle) httprouter.Handle {
 		return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
-			if r.Header.Get("Content-Type") != "application/json; charset=UTF-8" {
-
-				w.WriteHeader(http.StatusBadRequest)
-				w.Write([]byte(`{"message":"content type must be application/json; charset=UTF-8"}`))
-				return
-			}
 
 			if r.ContentLength > 0 {
 				if data, err := ioutil.ReadAll(r.Body); err == nil {
